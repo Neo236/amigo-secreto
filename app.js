c61e498eb9adb8f5ad8bd571794b3ce8807a1d85
Neo236 <lautarosebastianmambrin@hotmail.com>
2025-08-08 18:49:34 -0300

    Crea la funcion 'agregarAmigo'

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo()
+{
+    let nombreAmigoInput = document.getElementById('amigo');
+    let nombreAmigo = nombreAmigoInput.value.trim();
+
+    if (nombreAmigo === '')
+    {
+        alert("Por favor, inserte un nombre.");
+        return;
+    }
+
+    if (amigos.includes.nombreAmigo)
+    {
+        alert("Este nombre ya ha sido agregado. Por favor, inserte un nombre diferente.");
+        nombreAmigoInput.value = '';
+        return;
+    }
+
+    amigos.push(nombreAmigo);
+    let listaDeAmigos = document.getElementById('listaAmigos');
+    listaDeAmigos.textContent = amigos.join(', ');
+    nombreAmigoInput.value = '';
+    nombreAmigoInput.focus();
+}
+
